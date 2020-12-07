0c27960eba6f5945f31238ae8b1af5cbb1b38463
atrokhov <aatrokhov@gmail.com>
2020-12-07 21:00:23 +0600

    Initial commit

---
+++ homework1.py
@@ -0,0 +1,8 @@
+age = int(input("Введите возраст:\n"))
+
+if age >= 0 and age < 18:
+	print("Вы юноша")
+elif age >= 18 and age < 45:
+	print("Вы молодой")
+else:
+	print("Неправильно введён возраст")
